711a54c6af67814697e4349ef9c775f7a349099b
leonid-shutov <leonid.shutov.main@gmail.com>
2025-05-19 23:15:52 +0200

    fsp

--- oppress.js
+++ oppress.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const vm = require('node:vm');
-const fs = require('node:fs');
+const fsp = require('node:fs').promises;
 const path = require('node:path');
 
 const errors = require('./lib/errors.js');
@@ -11,14 +11,14 @@ const { Server } = require('./lib/server/server.js');
 const PATH = process.cwd();
 
 const readLayers = () =>
-  fs.readFile('.layers', 'utf8').then((data) => data.split(/[\r\n\s]+/).filter((s) => s.length !== 0));
+  fsp.readFile('.layers', 'utf8').then((data) => data.split(/[\r\n\s]+/).filter((s) => s.length !== 0));
 
 (async () => {
   const sandbox = { console, ...errors };
   const context = vm.createContext(sandbox);
   const load = loadDir.bind(undefined, context, context);
   const applicationPath = path.join(PATH, 'application');
-  for (const layer of readLayers()) await load(path.join(applicationPath, layer));
+  for (const layer of await readLayers()) await load(path.join(applicationPath, layer));
   const apis = Object.values(sandbox.api);
   const server = new Server(apis);
   server.start();
