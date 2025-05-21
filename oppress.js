5ee8f7cfb6a51c3c4acbb3dafab9f9ac28061da0
leonid-shutov <leonid.shutov.main@gmail.com>
2025-05-21 22:07:06 +0200

    move .layers to application

--- oppress.js
+++ oppress.js
@@ -9,15 +9,17 @@ const { loadDir } = require('./lib/loader.js');
 const { Server } = require('./lib/server/server.js');
 
 const PATH = process.cwd();
+const applicationPath = path.join(PATH, 'application');
 
 const readLayers = () =>
-  fsp.readFile('.layers', 'utf8').then((data) => data.split(/[\r\n\s]+/).filter((s) => s.length !== 0));
+  fsp
+    .readFile(path.join(applicationPath, '.layers'), 'utf8')
+    .then((data) => data.split(/[\r\n\s]+/).filter((s) => s.length !== 0));
 
 (async () => {
-  const sandbox = { console, ...errors };
+  const sandbox = { console, require, module, ...errors };
   const context = vm.createContext(sandbox);
   const load = loadDir.bind(undefined, context, context);
-  const applicationPath = path.join(PATH, 'application');
   for (const layer of await readLayers()) await load(path.join(applicationPath, layer));
   const apis = Object.values(sandbox.api);
   const server = new Server(apis);
